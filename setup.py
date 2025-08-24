03bab4b74de3ab01f2e196946873bfcc6702851b
Vidhi Bhutia <vidhibhutia2407@gmail.com>
2025-08-24 16:10:49 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="remedy_relay",
+    version="0.1.0",
+    author="Vidhi Bhutia",
+    author_email="vidhibhutia2407@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
